365cf607b824cc37cecfbc887333c93feec1fd9d
NiburuMe <niburume@gmail.com>
2023-01-14 14:04:37 +0100

    Initial commit

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -27,7 +27,7 @@ class WAppDrawer extends StatelessWidget {
           const Divider(),
           ListTile(
             leading: Icon(Icons.payment),
-            title: Text('Ordersss'),
+            title: Text('Orderssssss'),
             onTap: () {
               // Navigator.of(context)
               //     .pushReplacementNamed(OrdersScreen.routeName);
